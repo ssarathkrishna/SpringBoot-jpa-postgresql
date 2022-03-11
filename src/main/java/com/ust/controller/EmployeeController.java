862e38b7aa225a99b7dcba721a20fb1e4ee5a3f3
ssarathkrishna <S.Sarathkrishna@ust.com>
2022-03-11 11:55:42 +0530

    Logger added

--- src/main/java/com/ust/controller/EmployeeController.java
+++ src/main/java/com/ust/controller/EmployeeController.java
@@ -2,6 +2,8 @@ package com.ust.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,37 +20,42 @@ import com.ust.service.EmployeeService;
 @RestController()
 @RequestMapping("/employee")
 public class EmployeeController {
+	private static final Logger logger = LoggerFactory.getLogger(EmployeeController.class);
 
 	@Autowired
 	EmployeeService employeeService;
 
 	@PostMapping("/save")
 	private String saveEmployee(@RequestBody Employee employee) {
+		logger.info("inside save saveEmployee");
 		employeeService.save(employee);
 		return employee.getName() + " Saved Successfully";
 	}
 
 	@GetMapping("/getall")
 	private List<Employee> getEmployees() {
+		logger.info("inside getall getEmployees");
 		return employeeService.findAll();
 	}
 
 	@PutMapping("/update")
-	private String update(@RequestBody Employee employee) {
+	private String updateEmployee(@RequestBody Employee employee) {
+		logger.info("inside update updateEmployee");
 		employeeService.update(employee);
 		return employee.getName() + " Updated Successfully";
 	}
 
 	@DeleteMapping("/delete")
-	private String delete(@RequestBody Employee employee) {
+	private String deleteEmployee(@RequestBody Employee employee) {
+		logger.info("inside delete deleteEmployee");
 		employeeService.delete(employee);
 		return " deleted Successfully";
 	}
-	
+
 	@GetMapping("/customers")
 	private List<Customer> getCustomers() {
+		logger.info("inside customers getCustomers");
 		return employeeService.getCustomers();
 	}
-	
 
 }
--- src/main/java/com/ust/serviceimpl/EmployeeServiceImpl.java
+++ src/main/java/com/ust/serviceimpl/EmployeeServiceImpl.java
@@ -3,11 +3,14 @@ package com.ust.serviceimpl;
 import java.util.Arrays;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
 
+import com.ust.controller.EmployeeController;
 import com.ust.model.Customer;
 import com.ust.model.Employee;
 import com.ust.repository.EmployeeRepository;
@@ -15,6 +18,8 @@ import com.ust.service.EmployeeService;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
+	
+	private static final Logger logger = LoggerFactory.getLogger(EmployeeController.class);
 
 	private static final String CUSTOMERS = "/customer/getAll";
 
@@ -30,6 +35,7 @@ public class EmployeeServiceImpl implements EmployeeService {
 	@Override
 	public void save(Employee employee) {
 		// TODO Auto-generated method stub
+		logger.info("inside save saveEmployee");
 		employeeRepository.save(employee);
 
 	}
@@ -37,12 +43,14 @@ public class EmployeeServiceImpl implements EmployeeService {
 	@Override
 	public List<Employee> findAll() {
 		// TODO Auto-generated method stub
+		logger.info("inside  getEmployees");
 		return employeeRepository.findAll();
 	}
 
 	@Override
 	public void update(Employee employee) {
 		// TODO Auto-generated method stub
+		logger.info("inside  update");
 		employeeRepository.save(employee);
 
 	}
@@ -50,12 +58,13 @@ public class EmployeeServiceImpl implements EmployeeService {
 	@Override
 	public void delete(Employee employee) {
 		// TODO Auto-generated method stub
+		logger.info("inside  delete");
 		employeeRepository.deleteById(employee.getId());
 	}
 
 	@Override
 	public List<Customer> getCustomers() {
-
+		logger.info("inside  getCustomers");
 		Customer[] Customer = restTemplate.build().getForObject(mvcURL + CUSTOMERS, Customer[].class);
 		return Arrays.asList(Customer);
 	}
